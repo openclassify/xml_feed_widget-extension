8510e91596cbcce2fbfa06b149e55bbeb6390c01
Ryan Thompson <ryan@pyrocms.com>
2016-02-18 00:37:38 -0600

    Author

--- src/Command/SetContent.php
+++ src/Command/SetContent.php
@@ -10,9 +10,9 @@ use Illuminate\Contracts\Cache\Repository;
 /**
  * Class SetContent
  *
- * @link          http://anomaly.is/streams-platform
- * @author        AnomalyLabs, Inc. <hello@anomaly.is>
- * @author        Ryan Thompson <ryan@anomaly.is>
+ * @link          http://pyrocms.com/
+ * @author        PyroCMS, Inc. <support@pyrocms.com>
+ * @author        Ryan Thompson <ryan@pyrocms.com>
  * @package       Anomaly\XmlFeedWidgetExtension\Command
  */
 class SetContent implements SelfHandling
--- src/XmlFeedWidgetExtension.php
+++ src/XmlFeedWidgetExtension.php
@@ -8,9 +8,9 @@ use Anomaly\XmlFeedWidgetExtension\Command\SetContent;
 /**
  * Class XmlFeedWidgetExtension
  *
- * @link          http://anomaly.is/streams-platform
- * @author        AnomalyLabs, Inc. <hello@anomaly.is>
- * @author        Ryan Thompson <ryan@anomaly.is>
+ * @link          http://pyrocms.com/
+ * @author        PyroCMS, Inc. <support@pyrocms.com>
+ * @author        Ryan Thompson <ryan@pyrocms.com>
  * @package       Anomaly\XmlFeedWidgetExtension
  */
 class XmlFeedWidgetExtension extends WidgetExtension
